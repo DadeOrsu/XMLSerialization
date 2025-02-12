ea5fe9518241a1650a3cb4ce8bd96587e28bb8e5
DadeOrsu <dade.orsu@gmail.com>
2025-02-12 18:57:09 +0100

    added documentation.

--- src/XMLable.java
+++ src/XMLable.java
@@ -4,7 +4,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation to mark a class as XMLable.
+ * Annotation to indicate that a class can be serialized to XML.
+ * Classes marked with this annotation will be processed by the XMLSerializer.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
--- src/XMLfield.java
+++ src/XMLfield.java
@@ -3,12 +3,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 /**
- * Annotation to mark a field as XMLable.
+ * Annotation to indicate that a field should be serialized in XML.
+ * Fields marked with this annotation will be included in the XML output.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface XMLfield {
+    /**
+     * The Java type of the field.
+     */
     String type();
+    /**
+     * The name of the field in the XML output.
+     */
     String name() default "";
 }
